8c02aa765534d2561860c7d5f449a557ae70b087
Aryan Ranjan <aryanranjan@Aryans-MacBook-Air.local>
2024-06-04 20:22:56 +0400

    integrated payment page with razorpay

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -128,7 +128,7 @@ export const UserContextProvider = ({ children }) => {
                 loading,
                 registerUser,
                 verifyOtp,
-                // fetchUser,
+                fetchUser,
             }}
         >
             {children}
